f49dcce7a83bdad15de74db9f9b5b334d8a421fc
Your Name <your-email@example.com>
2025-11-02 20:39:58 +0100

    Complete migration to Strapi v5.3.0 with full API structure

---
+++ src/api/product/controllers/product.js
@@ -0,0 +1,7 @@
+/**
+ * product controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::product.product');
---
+++ src/api/product/services/product.js
@@ -0,0 +1,7 @@
+/**
+ * product service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::product.product');
