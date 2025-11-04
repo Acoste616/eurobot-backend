4ca405d6d561fbab3d86c7bb68c8c33b74d92af2
Your Name <your-email@example.com>
2025-11-04 11:38:20 +0100

    fix: Use Strapi v4 compatible dependency versions (react-router-dom 5.3.4, styled-components 5.3.11) and simplify i18n config

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,5 @@
 module.exports = ({ env }) => ({
-  // Enable i18n plugin with default locales
   i18n: {
     enabled: true,
-    config: {
-      locales: ['en', 'pl', 'de'],
-      defaultLocale: 'pl'
-    }
-  }
+  },
 });
